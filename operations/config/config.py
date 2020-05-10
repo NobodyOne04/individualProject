7014ad549e821b57ece997c8544c24637ecdebb4
v.shapovalov <nobody.one05111999@gmail.com>
2020-05-10 21:07:24 +0700

    add docs

--- operations/config/config.py
+++ operations/config/config.py
@@ -3,14 +3,14 @@ from configparser import ConfigParser
 
 class Config:
     def __init__(self):
-        self.__dataConfigObject = ConfigParser()
-        self.__dataConfigObject.read('config/project.conf')
-        self.__dataConfigObject.sections()
+        self.__configObject = ConfigParser()
+        self.__configObject.read('config/project.conf')
+        self.__configObject.sections()
 
     def get(self, category, key):
-        return self.__dataConfigObject.get(category, key)
+        return self.__configObject.get(category, key)
 
     def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(Config, cls).__new__(cls)
-        return cls.instance
+        if not hasattr(cls, '__instance'):
+            cls.__instance = super(Config, cls).__new__(cls)
+        return cls.__instance
